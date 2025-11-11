077821cb53d961a8132d23ed110189b4fb73bdaf
Salsabeel <58127260+sals1995@users.noreply.github.com>
2025-11-11 12:08:43 +0200

    adding lab files

---
+++ src/app/lab/hero-service/hero.lab.service.spec.ts
@@ -0,0 +1,5 @@
+describe("hero service (for lab) http testing:", () => {
+    
+    it("getHeroes function: send request and receive response successfully", () => { })
+    it("updateHero function: send request and receive response successfully", () => { })
+})
---
+++ src/app/lab/messages/messages.lab.spec.ts
@@ -0,0 +1,8 @@
+
+describe("message component (for lab) testing:", () => {
+
+    it("expect component template to be empty", () => {
+        //Note: there is @if"messageService.messages.length" in line 1 in template
+    })
+    it("then expect div.msg to have the messages after setting it", () => { })
+})
